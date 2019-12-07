73426a4055fc4a35fbad5ecfcaffbdc4e5941100
Ggouri <nalegave.gouri678@gmail.com>
2019-12-07 15:00:20 +0530

    done footer

--- vendors.php
+++ vendors.php
@@ -39,6 +39,7 @@ if(isset($_SESSION['userId'])){
         <input type="hidden" id="userId" value="<?php echo $userId;?>"/>
         <div class="wrapper">
             <?php include 'navbar.php';?>
+          
             <div class="page-wrap">
                 <?php include 'sidebar.php';?>
                 <div class="main-content">
@@ -112,12 +113,13 @@ if(isset($_SESSION['userId'])){
                         </div>
 
 
-                        <footer class="footer">
+                        <!-- <footer class="footer">
                             <div class="w-100 clearfix">
                                 <span class="text-center text-sm-left d-md-inline-block">Copyright © 2018 ThemeKit v2.0. All Rights Reserved.</span>
                                 <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark" target="_blank">Lavalite</a></span>
                             </div>
-                        </footer>
+                        </footer> -->
+                        <?php include 'footer.php';?>
                     </div>
                 </div>
 
