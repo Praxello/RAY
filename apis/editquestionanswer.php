af0f81bdffcf0d860fe1322665016f2c6fa95eee
vbpawar <vikaspawar3110@gmail.com>
2019-12-07 15:14:52 +0530

    added some changes

--- apis/editquestionanswer.php
+++ apis/editquestionanswer.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-include "../connection.php";
+include "../../connection.php";
 mysqli_set_charset($conn, 'utf8');
 $response = null;
 $records  = null;
--- apis/upload.php
+++ apis/upload.php
@@ -1,5 +1,7 @@
 <?php
-include "../connection.php";
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+include "../../connection.php";
 mysqli_set_charset($conn, 'utf8');
 $response   = null;
 $records    = null;
